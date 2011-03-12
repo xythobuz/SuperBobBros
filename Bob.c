af99a4a485e1f9562f385f9047aea87cf8f5de7c
Thomas Buck <taucher.bodensee@googlemail.com>
2011-03-12 02:26:32 +0100

    Stupidness Fix...

--- Bob.c
+++ Bob.c
@@ -104,17 +104,6 @@ int AddIn_main(int isAppli, unsigned short OptionNum) {
 
 	while(1) {
 		Bdisp_AllClr_DDVRAM();
-<<<<<<< HEAD
-		locate(6, 3);
-		Print((unsigned char*)"Super-Mario");
-		locate(9, 4);
-		Print((unsigned char*)"Clone");
-		locate(6, 6);
-		Print((unsigned char*)"by xythobuz");
-		locate(1, 8);
-		Print((unsigned char*)"F1:New Game");
-		Bdisp_PutDisp_DD(); //Draw buffer to display.
-=======
 		for (y = 0; y < 64; y++) {
 			for (x = 0; x < 128; x++) {
 				if (menuPixel(x, y) != 0) {
@@ -123,7 +112,6 @@ int AddIn_main(int isAppli, unsigned short OptionNum) {
 			}
 		}
 		Bdisp_PutDisp_DD();
->>>>>>> 6c865855a7cbc114968ae5682987658e89f6d366
 		GetKey(&key);
 		if (key == KEY_CTRL_F1) {
 			playLevel(1, 0);
