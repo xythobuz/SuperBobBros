a78d34348dfe48cc477305b4c2f7f6804335ef01
Max Nuding <the.short.german@gmail.com>
2011-03-30 16:46:12 +0200

    Whoopsy, added files for level 4 :)

---
+++ level4.c
@@ -0,0 +1,42 @@
+/*
+ * level 4
+ * generated with xythobuz' BobLevelCreator
+ */
+#include "level4.h"
+
+char lvl4_Block_0[31] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 28, 38, 39, 45 , -1 };
+char lvl4_Block_1[9] = { 0, 24, 26, 30, 31, 34, 38, 45 , -1 };
+char lvl4_Block_2[15] = { 0, 4, 13, 19, 20, 24, 32, 34, 38, 39, 40, 41, 42, 45 , -1 };
+char lvl4_Block_3[21] = { 0, 4, 12, 13, 18, 19, 23, 24, 26, 27, 29, 30, 32, 34, 35, 36, 37, 38, 44, 45 , -1 };
+char lvl4_Block_4[14] = { 0, 4, 6, 11, 12, 17, 19, 20, 24, 30, 32, 44, 45 , -1 };
+char lvl4_Block_5[25] = { 0, 4, 8, 10, 11, 12, 13, 14, 15, 19, 23, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 43, 45 , -1 };
+char lvl4_Block_6[10] = { 0, 4, 18, 19, 20, 26, 38, 42, 45 , -1 };
+char lvl4_Block_7[28] = { 0, 1, 2, 3, 4, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 29, 30, 31, 32, 39, 40, 41, 45 , -1 };
+
+char lvl4_Box_0[4] = { 25, 29, 33 , -1 };
+char lvl4_Box_1[2] = { 25 , -1 };
+char lvl4_Box_2[2] = { 25 , -1 };
+char lvl4_Box_3[4] = { 1, 25, 28 , -1 };
+char lvl4_Box_4[3] = { 3, 25 , -1 };
+char lvl4_Box_5[4] = { 1, 16, 25 , -1 };
+char lvl4_Box_6[1] = { -1 };
+char lvl4_Box_7[1] = { -1 };
+
+char lvl4_Coin_0[7] = { 26, 32, 34, 35, 36, 37 , -1 };
+char lvl4_Coin_1[4] = { 35, 36, 37 , -1 };
+char lvl4_Coin_2[5] = { 12, 35, 36, 37 , -1 };
+char lvl4_Coin_3[3] = { 8, 11 , -1 };
+char lvl4_Coin_4[7] = { 10, 13, 18, 29, 31, 36 , -1 };
+char lvl4_Coin_5[4] = { 17, 18, 24 , -1 };
+char lvl4_Coin_6[7] = { 13, 14, 15, 16, 17, 28 , -1 };
+char lvl4_Coin_7[1] = { -1 };
+
+
+char lvl4_Exits[2] = { 39, 1 };
+char *lvl4_Exit[1] = { lvl4_Exits };
+
+char *lvl4_Block[8] = { lvl4_Block_0, lvl4_Block_1, lvl4_Block_2, lvl4_Block_3, lvl4_Block_4, lvl4_Block_5, lvl4_Block_6, lvl4_Block_7 };
+char *lvl4_Box[8] = { lvl4_Box_0, lvl4_Box_1, lvl4_Box_2, lvl4_Box_3, lvl4_Box_4, lvl4_Box_5, lvl4_Box_6, lvl4_Box_7 };
+char *lvl4_Coin[8] = { lvl4_Coin_0, lvl4_Coin_1, lvl4_Coin_2, lvl4_Coin_3, lvl4_Coin_4, lvl4_Coin_5, lvl4_Coin_6, lvl4_Coin_7 };
+
+char **level4[4] = { lvl4_Block, lvl4_Box, lvl4_Coin, lvl4_Exit };
---
+++ level4.h
@@ -0,0 +1,6 @@
+/*
+ * level 4
+ * generated with xythobuz' BobLevelCreator
+ */
+
+extern char **level4[4];
